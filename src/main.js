bcb50b7f0e8b1d4e71d111f7ec1dc4c6311357e1
Shahriyar <shahryar.tarnasi@gmail.com>
2025-03-28 12:42:33 +0400

    countries info

--- src/main.js
+++ src/main.js
@@ -5,44 +5,64 @@ const btn = document.querySelector(".btn-country");
 const countriesContainer = document.querySelector(".countries");
 
 //////////////////////////////////////////////
-const getCountryData = function (country_name) {
+
+const renderCountry = function(data, className) {
+  const html = `
+    <article class="country ${className}">
+      <img class="country__img" alt="${data.flags.alt}" src="${data.flags.png}" />
+      <div class="country__data">
+        <h3 class="country__name">${data.name.common}</h3>
+        <h4 class="country__region">${data.region}</h3>
+        <p class="country__row"><span>👫</span>${(
+          +data.population / 1000000
+        ).toFixed(1)} people</p>
+        <p class="country__row"><span>🗣️</span>${
+          Object.entries(data.languages)[0][1]
+        }</p>
+        <p class="country__row"><span>💰</span>${Object.entries(data.currencies)[0][1].name}</p>
+      </div>
+    </article>
+  `;
+
+  countriesContainer.insertAdjacentHTML("beforeend", html);
+  countriesContainer.style.opacity = 1;
+}
+
+const getCountryAndNeighbour = function (country_name) {
   const request = new XMLHttpRequest();
   request.open("GET", `https://restcountries.com/v3.1/name/${country_name}`);
   request.send();
+
   request.addEventListener("load", function () {
     let data = JSON.parse(this.responseText);
 
-    if (Array.isArray(data)) {
-      data = data[0];
-    }
+    if (Array.isArray(data)) data = data[0];
     console.log(data);
 
-    const country_coin = Object.entries(data.currencies)[0];
+    // Render country
+    renderCountry(data)
 
-    const html = `
-      <article class="country">
-        <img class="country__img" alt="${data.flags.alt}" src="${
-      data.flags.png
-    }" />
-        <div class="country__data">
-          <h3 class="country__name">${data.name.common}</h3>
-          <h4 class="country__region">${data.region}</h3>
-          <p class="country__row"><span>👫</span>${(
-            +data.population / 1000000
-          ).toFixed(1)} people</p>
-          <p class="country__row"><span>🗣️</span>${
-            Object.entries(data.languages)[0][1]
-          }</p>
-          <p class="country__row"><span>💰</span>${country_coin[1].name}</p>
-        </div>
-      </article>
-    `;
+    // Get neighbour country
+    const [neighbour] = data.borders;
+    
+    if (!neighbour) return;
 
-    countriesContainer.insertAdjacentHTML("beforeend", html);
-    countriesContainer.style.opacity = 1;
+    // AJAX Call country 2
+    const request2 = new XMLHttpRequest();
+    request2.open('GET', `https://restcountries.com/v3.1/alpha/${neighbour.toLowerCase()}`)
+    request2.send();
+
+    request2.addEventListener('load', function() {
+      const [data2] = JSON.parse(this.responseText)
+      console.log(data2);
+      renderCountry(data2, 'neighbour')
+    })
   });
 };
 
-getCountryData("portugal")
-getCountryData("usa")
-getCountryData("germany")
+// constgetCountryAndNeighbour = function (country) {
+//   const request = new XMLHttpRequest();
+//   request.open('GET', 'https://restcountries.com/v3.1/)
+// }
+
+getCountryAndNeighbour("germany")
